8386561836d226dbbdea928fdff73904c7961f6a
jaapyang <15873115257@qq.com>
2017-11-08 00:04:36 +0800

    ### practice for chapter 1 - calculate the manhattan distance - coding unit test cases for manhattan

---
+++ src/Chapter_1/filteringdata.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+# -*-coding:utf-8-*-
+
+"""
+计算曼哈顿距离
+"""
+
+from math import sqrt
+
+
+def manhattan(rating1, rating2):
+    """
+    calculate the manhattan distance,
+    Both rating1 and rating2 are dictionaries of the form
+    {'the strokes':3.9,'Slightly Stoopid':4.5}
+    """
+    distance = 0
+    common_ratings = False
+    for key in rating1:
+        if key in rating2:
+            distance += abs(rating1[key] - rating2[key])
+            common_ratings = True
+    if common_ratings:
+        return distance
+    else:
+        return -1
+
+
+def calculate_nearest_neighbor(username, users):
+    """计算与指定用户距离最近的列表"""
+    distances = []
+    self_data = users[username]
+    for user in users:
+        if user != username:
+            distance = manhattan(users[user], self_data)
+            distances.append(distance)
+    distances.sort()
+    return distances
---
+++ src/tests/chapter_1.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+import unittest
+
+# -*-coding:utf-8-*-
+from unittest import TestCase
+
+from Chapter_1.filteringdata import *
+
+users = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0,
+                      "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
+         "Bill": {"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5,
+                  "Vampire Weekend": 3.0},
+         "Chan": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Deadmau5": 1.0, "Norah Jones": 3.0, "Phoenix": 5,
+                  "Slightly Stoopid": 1.0},
+         "Dan": {"Blues Traveler": 3.0, "Broken Bells": 4.0, "Deadmau5": 4.5, "Phoenix": 3.0, "Slightly Stoopid": 4.5,
+                 "The Strokes": 4.0, "Vampire Weekend": 2.0},
+         "Hailey": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0,
+                    "Vampire Weekend": 1.0},
+         "Jordyn": {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phoenix": 5.0, "Slightly Stoopid": 4.5,
+                    "The Strokes": 4.0, "Vampire Weekend": 4.0},
+         "Sam": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phoenix": 5.0,
+                 "Slightly Stoopid": 4.0, "The Strokes": 5.0},
+         "Veronica": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5,
+                      "The Strokes": 3.0}
+         }
+
+
+class TestFilteringData(unittest.TestCase):
+    def test_calculate_nearest_neighbor(self):
+        distances = calculate_nearest_neighbor("Bill", users)
+        expected = [4.0, 5.0, 5.5, 6.0, 8.0, 9.0, 14.0]
+        self.assertEqual(distances, expected)
+
+    def test_manhattan(self):
+        distance = manhattan(users['Sam'], users["Bill"])
+        expected = 8.0
+        self.assertEqual(distance, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
